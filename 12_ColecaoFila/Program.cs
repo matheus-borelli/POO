acf4e4cbd6858ca375358e984c310487041ee2ab
Matheus <matheussenac2026@gmail.com>
2025-09-26 19:56:27 -0300

    Fila de Atendimento com espera

--- 12_ColecaoFila/Program.cs
+++ 12_ColecaoFila/Program.cs
@@ -1,24 +1,80 @@
 ﻿using System;
 using System.Collections.Generic;
-
-namespace _13_ColecaoPilha
+ 
+namespace ColecaoFila
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Stack<int> pilha = new Stack<int>();
-
-            Console.WriteLine(" PILHA ");
-
-            for (int i = 0; i < 5; i++)
+            Queue<string> fila = new Queue<string>();
+            int totalPessoas = 0;
+ 
+            Console.WriteLine("Fila de Atendimento");
+            Console.Write("Quantas pessoas você quer adicionar à fila? ");
+ 
+            
+            while (true)
             {
-                Console.Write($"Digite o {i + 1}º número: ");
-                int numero = int.Parse(Console.ReadLine());
-                pilha.Push(numero);// push enumera as pilhas
+                if (int.TryParse(Console.ReadLine(), out totalPessoas) && totalPessoas >= 3)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.Write("Digite um número válido (mínimo 3): ");
+                }
             }
-
-            Console.WriteLine($"\nO último número inserido foi: {pilha.Peek()}");
+ 
+            
+            for (int i = 1; i <= totalPessoas; i++)
+            {
+                while (true)
+                {
+                    Console.Write($"Digite o nome da pessoa {i}: ");
+                    string nome = Console.ReadLine();
+ 
+                    if (!string.IsNullOrWhiteSpace(nome) && nome.Length >= 3)
+                    {
+                        fila.Enqueue(nome);
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nome inválido. O nome deve ter no mínimo 3 caracteres.");
+                    }
+                }
+            }
+ 
+            Console.WriteLine("\nAtendimento:");
+            for (int i = 1; i <= 3; i++)
+            {
+                if (fila.Count > 0)
+                {
+                    string atendido = fila.Dequeue();
+                    Console.WriteLine($"{i}º atendido: {atendido}");
+                }
+                else
+                {
+                    Console.WriteLine($"{i}º atendido: (ninguém na fila)");
+                }
+            }
+ 
+           
+            Console.WriteLine("\nQuem ainda está aguardando na fila:");
+            if (fila.Count == 0)
+            {
+                Console.WriteLine("Nenhuma pessoa aguardando.");
+            }
+            else
+            {
+                foreach (string pessoa in fila)
+                {
+                    Console.WriteLine($"- {pessoa}");
+                }
+            }
         }
     }
 }
+
+ 
