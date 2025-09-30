e9fbf022a6eda8a2763a463d4e56c1be26141695
Matheus <matheussenac2026@gmail.com>
2025-09-29 22:20:27 -0300

    Enumeration

---
+++ 17_Calendario/Feriado.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17_Calendario
+{
+    internal class Feriado
+    {
+        public int dia {  get; set; }
+        public string Descricao { get; set; }
+        public int Dia { get; internal set; }
+
+        public Feriado(int dia, string descricao)
+        {
+            this.dia = dia;
+            this.Descricao = descricao;
+        }
+    }
+}
---
+++ 17_Calendario/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace _17_Calendario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.Write("Digite o ano:");
+            int ano = int.Parse(Console.ReadLine());
+
+            Calendario calendario;
+            for (int mes = 1; mes <= 12; mes++)
+            {
+                
+                calendario = new Calendario(ano, (Mes)mes);
+                calendario.ImprimirCalendario();
+
+                Console.WriteLine("\n\n");
+
+            }
+            //Espera o usuário teclar qualquer tecla
+            Console.ReadKey();
+
+
+        } 
+    }
+}
