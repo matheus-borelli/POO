547e077f30f3450cc93d985974f38351da0e3ff8
Matheus <matheussenac2026@gmail.com>
2025-10-06 20:42:45 -0300

    Tratamento de Exceção

---
+++ 19_TratamentoExcecao/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19_TratamentoExcecao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            while (true)
+            {
+                Console.WriteLine("Digite um número inteiro: ");
+                try
+                {
+                    numero = int.Parse(Console.ReadLine());
+                    break;
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("Número inválido, informe apenas valores!\nPressione qualquer tecla para continuar");
+                    Console.ReadKey();
+                }
+                finally 
+                {
+                    Console.Clear();
+                }
+               
+                
+
+               
+            }
+
+            if (numero % 2 == 0)
+                Console.WriteLine("Número par!");
+            else
+                Console.WriteLine("Número ímpar!");
+
+
+        }
+    }
+}
